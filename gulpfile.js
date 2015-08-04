611cae8df7851b49b30a177b07b698722a4f7426
John Stringer <jstringer@imodules.com>
2015-08-04 11:29:01 -0500

    changed babel task in gulpfile to babelTest-updated tc build config

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 
-gulp.task("default", function () {
+gulp.task("babelTest", function () {
   return gulp.src("src/test.js")
     .pipe(babel())
     .pipe(gulp.dest("dist"));
